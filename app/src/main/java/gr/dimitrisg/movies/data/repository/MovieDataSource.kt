b8108bd16e7fedb25eeb8e3044323ee87e04af93
Dimitris Gournias <dig@entersoft.gr>
2020-04-29 17:49:45 +0300

    Implement page key class.

---
+++ app/src/main/java/gr/dimitrisg/movies/data/repository/MovieDataSource.kt
@@ -0,0 +1,70 @@
+package gr.dimitrisg.movies.data.repository
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.paging.PageKeyedDataSource
+import gr.dimitrisg.movies.data.api.FIRST_PAGE
+import gr.dimitrisg.movies.data.api.RestApi
+import gr.dimitrisg.movies.data.model.popularmovie.Movie
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+
+class MovieDataSource(
+    private val apiService: RestApi,
+    private val compositeDisposable: CompositeDisposable
+) : PageKeyedDataSource<Int, Movie>() {
+
+    private var page = FIRST_PAGE
+
+    val networkState: MutableLiveData<NetworkState> = MutableLiveData()
+
+    override fun loadInitial(
+        params: LoadInitialParams<Int>,
+        callback: LoadInitialCallback<Int, Movie>
+    ) {
+        networkState.postValue(NetworkState.LOADING)
+
+        compositeDisposable
+            .add(
+                apiService
+                .getPopularMovie(page)
+                .subscribeOn(Schedulers.io())
+                .subscribe(
+                {
+                    callback.onResult(it.movieList, null, page + 1)
+                    networkState.postValue(NetworkState.LOADED)
+                },
+                {
+                    networkState.postValue(NetworkState.ERROR)
+                }
+            )
+        )
+    }
+
+    override fun loadAfter(params: LoadParams<Int>, callback: LoadCallback<Int, Movie>) {
+        networkState.postValue(NetworkState.LOADING)
+        val key = params.key
+
+        compositeDisposable
+            .add(apiService.getPopularMovie(key)
+                .subscribeOn(Schedulers.io())
+                .subscribe(
+                    {
+                        if(it.totalPages >= params.key){
+                            callback.onResult(it.movieList,  key + 1)
+                            networkState.postValue(NetworkState.LOADED)
+                        } else {
+                            networkState.postValue(NetworkState.ENDOFLIST)
+                        }
+                    },
+                    {
+                        networkState.postValue(NetworkState.ERROR)
+                    }
+                )
+            )
+    }
+
+    override fun loadBefore(params: LoadParams<Int>, callback: LoadCallback<Int, Movie>) {
+
+    }
+}
