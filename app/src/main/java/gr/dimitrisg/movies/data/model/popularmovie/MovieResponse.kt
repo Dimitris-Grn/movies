c81301825a23efd1f989803ccf14b70cfd111f2b
Dimitris Gournias <dig@entersoft.gr>
2020-04-30 12:40:09 +0300

    Task completed. Must carefull the response types because not return correct result.

--- app/src/main/java/gr/dimitrisg/movies/data/model/popularmovie/MovieResponse.kt
+++ app/src/main/java/gr/dimitrisg/movies/data/model/popularmovie/MovieResponse.kt
@@ -4,7 +4,6 @@ package gr.dimitrisg.movies.data.model.popularmovie
 import com.google.gson.annotations.SerializedName
 
 data class MovieResponse(
-    @SerializedName("page")
     val page: Int,
     @SerializedName("results")
     val movieList: List<Movie>,
--- app/src/main/java/gr/dimitrisg/movies/ui/popular_movie/MainActivity.kt
+++ app/src/main/java/gr/dimitrisg/movies/ui/popular_movie/MainActivity.kt
@@ -3,13 +3,73 @@ package gr.dimitrisg.movies.ui.popular_movie
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import gr.dimitrisg.movies.R
+import gr.dimitrisg.movies.data.api.RestApi
+import gr.dimitrisg.movies.data.api.RestClient
+import gr.dimitrisg.movies.data.repository.NetworkState
+import gr.dimitrisg.movies.data.viemwodel.MainActivityViewModel
+import gr.dimitrisg.movies.data.viemwodel.SingleMovieViewModel
 import gr.dimitrisg.movies.ui.single_movie_details.SingleMovie
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainActivityViewModel
+
+    lateinit var movieRepository: MoviePagedListRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val apiService : RestApi = RestClient.getClient()
+        movieRepository = MoviePagedListRepository(apiService)
+
+        viewModel = getViewModel()
+
+        val movieAdapter = PopularMoviePagedListAdapter(this)
+
+        val gridLayoutManager = GridLayoutManager(this, 3)
+
+        gridLayoutManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup(){
+            override fun getSpanSize(position: Int): Int {
+                val viewType = movieAdapter.getItemViewType(position)
+
+                if(viewType == movieAdapter.MOVIE_VIEW_TYPE) return 1
+                else return 3
+            }
+        }
+
+        rv_movie_list.layoutManager = gridLayoutManager
+        rv_movie_list.setHasFixedSize(true)
+        rv_movie_list.adapter = movieAdapter
+
+        viewModel.moviePageList.observe(this, Observer {
+            movieAdapter.submitList(it)
+        })
+
+        viewModel.networkState.observe(this, Observer {
+            progress_bar_popular.visibility = if(viewModel.listIsEmpty() && it == NetworkState.LOADING) View.VISIBLE else View.GONE
+            txt_error_popular.visibility = if(viewModel.listIsEmpty() && it == NetworkState.ERROR) View.VISIBLE else View.GONE
+
+            if(!viewModel.listIsEmpty()) {
+                movieAdapter.setNetworkState(it)
+            }
+        })
+    }
+
+    // Otan theloyme na pairname constructor parameters mesa sto VM
+    private fun getViewModel(): MainActivityViewModel {
+        return ViewModelProvider(this, object : ViewModelProvider.Factory {
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                @Suppress("UNCHECKED_CAST")
+                return MainActivityViewModel(movieRepository) as T
+            }
+        }).get(MainActivityViewModel::class.java)
     }
 }
